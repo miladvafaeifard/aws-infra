bebd31e19df78ee8171e29773ac0e5053d9c41a6
Milad Vafaeifard <milad.vafaeifard@gmail.com>
2024-12-17 16:34:00 +0100

    Complete Serverless task

--- bin/aws-infra.ts
+++ bin/aws-infra.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { AwsInfraStack } from '../lib/aws-infra-stack';
+import { DeployWebAppStack } from '../lib/deploy-web-app-stack';
 
 const app = new cdk.App();
-new AwsInfraStack(app, 'AwsInfraStack', {
+new DeployWebAppStack(app, 'DeployWebAppStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
